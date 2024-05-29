e45fc09757bfe7f9fd303cb682b5bf0cbb47959e
PiyushGhorela <156682649+PiyushGhorela@users.noreply.github.com>
2024-05-30 01:18:53 +0530

    Create Piyush.js

---
+++ Piyush.js
@@ -0,0 +1,58 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+
+const NFTs = []
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+
+function mintNFT ( _name, _haircolor, _moonsign , _bling) {
+       const NFT = {
+         "name" : _name,
+         "haircolor" : _haircolor,
+         "moonsign" :   _moonsign ,
+         "bling" : _bling
+       }
+       NFTs.push(NFT);
+        console.log("Minted: " + _name);  
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+
+function listNFTs () {
+   for(let i = 0; i< NFTs.length; i++){
+       console.log("\nID         : " + (i+1));
+       console.log("Name       : " + NFTs[i].name);
+       console.log("Haircolor  : " + NFTs[i].haircolor);
+       console.log("Moon Sign  : " + NFTs[i].moonsign);
+       console.log("Bling      : " + NFTs[i].bling);
+   }
+}
+
+// print the total number of NFTs we have minted to the console
+
+function getTotalSupply() {
+console.log("\nTotal NFTs : " + NFTs.length);
+}
+
+// call your functions below this line
+
+mintNFT("Piyush", "Black", "Scorpio", "Silver chain ");
+mintNFT("Anshika", "Black", "Cancer", "Golden chain ");
+mintNFT("Aditya", "Grey", "Tauras", "Ring ");
+mintNFT("Abhinav", "Blonde", "Cancer", "Bracelete");
+
+listNFTs();
+
+getTotalSupply();
